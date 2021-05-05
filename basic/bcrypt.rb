e9cc3e868d45a74c6c5e8a3c24d192ab81de1bcd
beltranbot <beltranbot@gmail.com>
2021-05-04 20:50:02 -0500

    finished OOP section

--- basic/bcrypt.rb
+++ basic/bcrypt.rb
@@ -1,8 +1,35 @@
-require "bcrypt"
+require 'bcrypt'
 
-my_password = BCrypt::Password.create("my password")
+users = [
+  { username: 'mashrur', password: 'password1' },
+  { username: 'jack', password: 'password2' },
+  { username: 'arya', password: 'password3' },
+  { username: 'jonshow', password: 'password4' },
+  { username: 'heisenberg', password: 'password5' }
+]
 
-p my_password.version
-p my_password.cost
-p my_password == "my password"
-p my_password == "not my password"  
+def create_hash_digest(password)
+  BCrypt::Password.create(password)
+end
+
+def verify_hash_disgest(password)
+  BCrypt::Password.new(password)
+end
+
+def create_secure_users(list_of_users)
+  list_of_users.each do |user_record|
+    user_record[:password] = create_hash_digest(user_record[:password])
+  end
+  list_of_users
+end
+
+new_users = create_secure_users(users)
+
+def authenticate_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    return user_record if user_record[:username] == username && verify_hash_disgest(user_record[:password]) == password
+  end
+  'Credentials were not correct'
+end
+
+puts authenticate_user('heisenberg', 'password5', new_users)
